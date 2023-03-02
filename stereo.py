0fb23e5274f11bbbad8d85ed02a6f534aee453f0
Sporillium <straussmarch@gmail.com>
2023-03-02 11:32:45 +0200

    Added class definition documentation to stereoExtractor

--- stereo.py
+++ stereo.py
@@ -13,6 +13,44 @@ from yaml.loader import SafeLoader
 
 # ----- Class Definitions -----
 class StereoExtractor:
+    """
+    Initialises a StereoExtractor Object, which is used analyse stereo image pairs and extract 3D features from them.
+
+    Parameters:
+    ----------
+        detector: Specifies which 2D feature extractor to use for extracting features from the 2D images. Options include SIFT, SURF, and ORB.
+                    Default value: 'SIFT'
+        
+        matcher: Specifies which method to use to match extracted features from the left and right images. Options are brute force (BF) or FLANN (NN).
+                    Default value: 'BF'
+        
+        camera_id: Specifies which camera settings to use from camera_info.yaml. 0 = KITTI
+                    Default value: 0
+
+        seq: Specifies which sequence (if present) that the system should process. For image systems with only one sequence, None is specified.
+                Default value: None 
+
+    Instance Variables:
+    ----------
+        detector_id: String
+        matcher_id: String
+        camera_id: int
+
+        cam_name: String
+        cam_baseline: float
+        cam_fl: float
+        cam_xoff: float
+        cam_y_off: float
+
+        path_l: String
+        path_r: String
+        poses: String
+        seq_len: int
+                    
+    Returns:
+    ----------
+        Instance of Stereo Extractor object object
+    """
     def __init__(self, detector='SIFT', matcher='BF', camera_id=0, seq=None):
         self.detectorID = detector
         self.matcherID = matcher
