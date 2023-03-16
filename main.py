63146dc91e348ddf963e6269af162ca8efc6d7d0
Sporillium <straussmarch@gmail.com>
2023-03-16 14:26:07 +0200

    Added first-frame cloud generation

--- main.py
+++ main.py
@@ -11,10 +11,22 @@ import numpy as np
 import cv2 as cv
 
 # ----- Main Execution Starts here -----
+# Load processing objects:
 segmentation_engine = seg.SegmentationEngine(model_id=6, use_gpu=False)
-
 stereo_extractor = stereo.StereoExtractor(segmentation_engine, detector='SIFT', matcher='BF', camera_id=0, seq=0)
-
 cloud_engine = cloud.CloudProcessor(stereo_extractor)
 
-print(type(cloud_engine.stereo_extractor))
+# Define image regions:
+IM_START = 0
+IM_END = IM_START + 5
+
+single_frames = {}
+pose_changes = {}
+point_cloud = []
+
+single_frames[IM_START] = cloud_engine.processFrame(IM_START)
+for point in single_frames[IM_START]:
+    point.frame_num = IM_START
+
+print("COMPLETED")
+print(len(single_frames[IM_START]))
