fcacc4b585cc665b82696487297d4b5c4f05c08a
Sporillium <straussmarch@gmail.com>
2023-04-20 17:02:07 +0200

    Added argparse switches to script

--- loop_closure.py
+++ loop_closure.py
@@ -4,71 +4,56 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 from scipy.spatial import distance_matrix
 import cv2 as cv
+import argparse
 
-match_boundary = 50 # Number of frames before/after matching can occur
-gt_threshold = 5.0
+# Argument Parser:
+parser = argparse.ArgumentParser(description="Detect Loop Closures using M2DP Descriptor")
+parser.add_argument('-n', '--number', required=True, type=int, help="Specify sequence number to be processes") # Sequence Number
+parser.add_argument('-r', '--use_regular', action='store_true', help="Set flag to use visual-based descriptors") # Visual Descriptors
+parser.add_argument('-s', '--use_sem', action='store_true', help="Set flag to use visual-semantic based descriptors") #Semantic-Visual Descriptors
+parser.add_argument('-v', '--use_velodyne', action='store_true', help="Set flag to use Velodyne-based descriptors") #Use Velo True/False
+parser.add_argument('-c', '--use_velo_sem', action='store_true', help="Set flag to use Velodyne-Semantic based descriptors") #Use VeloSem true_false
 
-SEQ_NUM = 6
+args = parser.parse_args()
+
+# Define Execution flags:
+SEQ_NUM = args.number
+
+USE_REG = args.use_regular
+USE_SEM = args.use_sem
+USE_VELO = args.use_velodyne
+USE_VELO_SEM = args.use_velo_sem
+
 seq_name = f'{SEQ_NUM:02}'
 
+if SEQ_NUM > 10:
+    print("NO GROUND TRUTH AVAILABLE FOR SEQUENCE " +seq_name+". PLEASE USE DIFFERENT SCRIPT")
+    exit()
+
+if (USE_REG or USE_SEM or USE_VELO or USE_VELO_SEM) is False:
+    print("AT LEAST ONE DESCRIPTOR TYPE MUST BE USED. PLEASE SEE HELP FOR DETAILS")
+    exit()
+
+match_boundary = 50 # Number of frames before/after matching can occur
+gt_threshold = 10.0
 SEARCH_RANGE = 2.0
-SEARCH_INTERVAL = 0.001
+SEARCH_INTERVAL = 0.002
 
 matcher = cv.BFMatcher_create(cv.NORM_L2)
 
-# Load Poses for GT Loop closures
-poses = np.loadtxt('/home/march/devel/datasets/Kitti/odometry-2012/poses/'+seq_name+'.txt')
-locations = np.zeros((poses.shape[0], 3))
-for i in range(poses.shape[0]):
-    base_pose = np.reshape(poses[0, :], (3,4))
-    pose = np.reshape(poses[i, :], (3,4))
-    locations[i, :] = pose[:,3]
-print("GT POSES LOADED")
-distances = distance_matrix(locations, locations)
-
-#plt.imshow(distances, cmap='hot')
-
-
-seq_len = len(locations)
-
-# Load Pre-computed Descriptors for Evaluation
-descriptors = np.zeros((seq_len, 192))
-with open('descriptor_texts/velo_descriptors_kitti_'+seq_name+'.txt', 'r') as file:
-    lines = file.readlines()
-for i, line in zip(range(len(lines)), lines):
-    try:
-        des = np.fromstring(line.strip('[]\n'), sep=';')
-        descriptors[i, :] = des
-    except:
-        print(i)
-        print(line)
-print("DESCRIPTORS LOADED")
-descriptors = descriptors.astype(np.float32)
-
-cloud_ids = np.arange(seq_len)
-
-# Define Lists for Curve:
-precision = []
-recall = []
-
-# Loop through distance Thresholds:
-thresholds = np.arange(0.0, SEARCH_RANGE, SEARCH_INTERVAL)
-for thresh in tqdm(thresholds):
-
+def evaluate_match(input_descriptor, cloud_ids, distances):
     # Define values:
     tp = 0
     tn = 0
     fp = 0
     fn = 0
-
     for id in cloud_ids:
-        base_cloud = descriptors[id,:].reshape((1,192))
-        
+        base_cloud = input_descriptor[id,:].reshape((1,192))    
         # Create valid matching set:
         if id-match_boundary <= 0:
             continue
         else:
-            valid_descriptors = descriptors[:id-match_boundary, :]
+            valid_descriptors = input_descriptor[:id-match_boundary, :]
             valid_ids = cloud_ids[:id-match_boundary]
         
         # Run matcher
@@ -91,13 +76,117 @@ for thresh in tqdm(thresholds):
                 fn += 1
             else:
                 tn += 1
-    #print(tp, fp, tn, fn)
-    # Calculate Precision and recall values:
-    prec = tp/(tp+fp)
-    rec = tp/(tp+fn)
+    try:
+        prec = tp/(tp+fp)
+    except:
+        prec = 1
+    
+    try:
+        rec = tp/(tp+fn)
+    except:
+        rec = 0
 
-    precision.append(prec)
-    recall.append(rec)
+    return prec, rec
+
+# Load Poses for GT Loop closures
+poses = np.loadtxt('/home/march/devel/datasets/Kitti/odometry-2012/poses/'+seq_name+'.txt')
+locations = np.zeros((poses.shape[0], 3))
+for i in range(poses.shape[0]):
+    base_pose = np.reshape(poses[0, :], (3,4))
+    pose = np.reshape(poses[i, :], (3,4))
+    locations[i, :] = pose[:,3]
+print("GT POSES LOADED")
+distances = distance_matrix(locations, locations)
+seq_len = len(locations)
+
+
+# Load Pre-computed Descriptors for Evaluation based on Flags:
+if USE_REG:  
+    descriptors_reg = np.zeros((seq_len, 192))
+    with open('descriptor_texts/basic_descriptors_kitti_'+seq_name+'.txt', 'r') as file:
+        lines = file.readlines()
+    for i, line in zip(range(len(lines)), lines):
+        try:
+            des = np.fromstring(line.strip('[]\n'), sep=';')
+            descriptors_reg[i, :] = des
+        except:
+            print(i)
+            print(line)
+    print("VISUAL DESCRIPTORS LOADED")
+    descriptors_reg = descriptors_reg.astype(np.float32)
+    precision_reg = []
+    recall_reg = []
+
+if USE_SEM:  
+    descriptors_sem = np.zeros((seq_len, 192))
+    with open('descriptor_texts/sem_descriptors_kitti_'+seq_name+'.txt', 'r') as file:
+        lines = file.readlines()
+    for i, line in zip(range(len(lines)), lines):
+        try:
+            des = np.fromstring(line.strip('[]\n'), sep=';')
+            descriptors_sem[i, :] = des
+        except:
+            print(i)
+            print(line)
+    print("VISUAL-SEMANTIC DESCRIPTORS LOADED")
+    descriptors_sem = descriptors_sem.astype(np.float32)
+    precision_sem = []
+    recall_sem = []
+
+if USE_VELO:  
+    descriptors_velo = np.zeros((seq_len, 192))
+    with open('descriptor_texts/velo_descriptors_kitti_'+seq_name+'.txt', 'r') as file:
+        lines = file.readlines()
+    for i, line in zip(range(len(lines)), lines):
+        try:
+            des = np.fromstring(line.strip('[]\n'), sep=';')
+            descriptors_velo[i, :] = des
+        except:
+            print(i)
+            print(line)
+    print("VELODYNE DESCRIPTORS LOADED")
+    descriptors_velo = descriptors_velo.astype(np.float32)
+    precision_velo = []
+    recall_velo = []
+
+if USE_VELO_SEM:  
+    descriptors_velo_sem = np.zeros((seq_len, 192))
+    with open('descriptor_texts/velo_sem_descriptors_kitti_'+seq_name+'.txt', 'r') as file:
+        lines = file.readlines()
+    for i, line in zip(range(len(lines)), lines):
+        try:
+            des = np.fromstring(line.strip('[]\n'), sep=';')
+            descriptors_velo_sem[i, :] = des
+        except:
+            print(i)
+            print(line)
+    print("VELODYNE DESCRIPTORS LOADED")
+    descriptors_velo_sem = descriptors_velo_sem.astype(np.float32)
+    precision_velo_sem = []
+    recall_velo_sem = []
+
+cloud_ids = np.arange(seq_len)
+
+# Loop through distance Thresholds:
+thresholds = np.arange(0.0, SEARCH_RANGE, SEARCH_INTERVAL)
+for thresh in tqdm(thresholds):
+    if USE_REG:
+        prec_reg, rec_reg = evaluate_match(descriptors_reg, cloud_ids, distances)
+        precision_reg.append(prec_reg)
+        recall_reg.append(rec_reg)
+    if USE_SEM:
+        prec_sem, rec_sem = evaluate_match(descriptors_sem, cloud_ids, distances)
+        precision_sem.append(prec_sem)
+        recall_sem.append(rec_sem)
+    if USE_VELO:
+        prec_velo, rec_velo = evaluate_match(descriptors_velo, cloud_ids, distances)
+        precision_velo.append(prec_velo)
+        recall_velo.append(rec_velo)
+    if USE_VELO_SEM:
+        prec_velo_sem, rec_velo_sem = evaluate_match(descriptors_velo_sem, cloud_ids, distances)
+        precision_velo_sem.append(prec_velo_sem)
+        recall_velo_sem.append(rec_velo_sem)
+    
 
 # Plot the Curve   
 fig = plt.figure()
@@ -106,21 +195,19 @@ ax.set_aspect('equal')
 ax.set_title("Precision - Recall Curve")
 ax.set_xlabel("Recall")
 ax.set_ylabel("Precision")
-ax.plot(recall, precision,  'b-')
+if USE_REG:
+    ax.plot(recall_reg, precision_reg,  'b-')
+if USE_SEM:
+    ax.plot(recall_sem, precision_sem,  'g-')
+if USE_VELO:
+    ax.plot(recall_velo, precision_velo,  'r-')
+if USE_VELO_SEM:
+    ax.plot(recall_velo_sem, precision_velo_sem,  'y-')
 ax.grid()
-ax.set_xlim([0.0, 1.0])
-ax.set_ylim([0.0, 1.0])
-
-# path = plt.figure()
-# ax1 = path.add_subplot(111, projection='3d')
-# ax1.set_title("GT Path")
-# #ax1.set_box_aspect((np.ptp(locations[:,0]), np.ptp(locations[:,1]), np.ptp(locations[:,2])))
-# ax1.scatter(locations[:,0], locations[:,1], locations[:,2])
-# ax1.set_aspect('equal')
-# ax1.set_xlabel("X Axis")
-# ax1.set_ylabel("Y Axis")
-# ax1.set_zlabel("Z Axis")
+ax.set_xlim([-0.01, 1.01])
+ax.set_ylim([-0.01, 1.01])
 
 
 plt.show()
+
     
