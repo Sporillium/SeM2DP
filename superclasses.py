bcf61cf9a7236638f8502122939e5a91b7b7a23d
Sporillium <straussmarch@gmail.com>
2023-06-13 11:32:44 +0200

    Defined groupings for superclasses

---
+++ superclasses.py
@@ -0,0 +1,62 @@
+# File for defining Superclass groupings:
+
+#-1
+void_class = [-1]
+'''Void'''
+# Total Weight in Dataset: 0.0725 (7.25%)
+
+#0
+structures_class = [0, 1, 8, 14, 25, 42, 48, 53, 58, 59, 61, 77, 79, 84, 95, 96, 101, 109, 121, 122, 140]
+'''Wall, Building, Window, Door, House, Column, Skyscraper, Stairs, Screen Door, Stariway, Bridge, Bar, Hovel, Tower, Bannister, Escalator,
+    Stage, Swimming Pool, Step, Tank, Pier,'''
+# Total Weight in Dataset: 0.3171 (31.71%)
+
+#1
+vehicles_class = [20, 76, 80, 83, 90, 102, 103, 116, 127]
+'''Car, Boat, Bus, Truck, Airplane, Van, Ship, Minibike, Bicycle '''
+# Total Weight in Dataset: 0.0152 (1.52%)
+
+#2
+environment_class = [9, 13, 16, 21, 26, 29, 46, 60, 68, 113, 128]
+'''Grass, Earth, Mountain, Water, Sea, Field, Sand, River, Hill, Waterfall, Lake'''
+# Total Weight in Dataset: 0.0669 (6.69%)
+
+#3
+furniture_class = [7, 10, 15, 19, 23, 24, 30, 31, 33, 35, 37, 44, 45, 47, 49, 50, 56, 62, 64, 65, 70, 71, 73, 75, 78, 88, 97, 99, 107, 110, 117, 118, 129, 133, 145, 146]
+'''Bed, Cabinent, Table, Chair, Sofa, Shelf, Armchair, Seat, Desk, Wardrobe, Bathtub, Chest of Drawers, Counter, Sink, Fireplace, Refrigerator,
+    Pool Table, Bookcase, Coffee Table, Toilet, Countertop, Stove, Kitchen Island, Swivel Chair, Arcade Machine, Booth, Ottoman, Buffet Counter,
+    Wacher, Stool, Cradle, Oven, Dishwasher, Exhaust Hood, Shower, Radiator'''
+# Total Weight in Dataset: 0.1168 (11.8%)
+
+#4
+exterior_object_class = [32, 38, 43, 51, 69, 86, 87, 93, 104, 106, 111, 114, 119, 123, 125, 132, 136, 144, 149]
+'''Fence, Railing, Signboard, Grandstand, Bench, Awning, Streetlight, Pole, Fountain, Canopy, Barrel, Tent, Ball, Brand Name, Pot, Sculpture,
+    Traffic Light, Notice Board, Flag'''
+# Total Weight in Dataset: 0.0173 (1.73%)
+
+#5
+natural_object_class = [4, 17, 34, 66, 72]
+'''Tree, Plant, Rock, Flower, Palm Tree'''
+# Total Weight in Dataset: 0.064 (6.4%)
+
+#6
+interior_object_class = [18, 22, 27, 28, 36, 39, 40, 41, 55, 57, 63, 67, 74, 81, 82, 85, 89, 92, 98, 100, 105, 108, 112, 115, 120, 124, 130, 131, 134, 135, 137, 138, 139, 141, 142, 143, 147, 148]
+'''Curtain, Painting, Mirror, Rug, Lamp, Cushion, Base, Box, Case, Pillow, Blind, Book, Computer, Towel, Light, Chandelier Television,
+    Apparel, Bottle, Poster, Conveyor Belt, Toy, Basket, Bag, Food, Microwave, Screen, Blanket, Sconce, Vase, Tray, Ashcan, Fan, CRT Screen,
+    Plate, Monitor, Drinking Glass, Clock'''
+# Total Weight in Dataset: 0.055 (5.5%)
+
+#7
+flat_surface_class = [3, 5, 6, 11, 52, 54, 91, 94]
+'''Floor, Ceiling, Road, Sidewalk, Path, Runway, Dirt Track, Land, '''
+# Total Weight in Dataset: 0.1683 (16.83%)
+
+#8
+sky_class = [2]
+'''Sky'''
+# Total Weight in Dataset: 0.0878 (8.78%)
+
+#9
+creatures_class = [12, 126]
+'''Person, Animal'''
+# Total Weight in Dataset: 0.0164 (1.64%)
