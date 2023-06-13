c16325f533e276ed901e9741b819c1e012450642
Sporillium <straussmarch@gmail.com>
2023-06-13 11:40:39 +0200

    Reordered classes by total weight

--- superclasses.py
+++ superclasses.py
@@ -12,27 +12,26 @@ structures_class = [0, 1, 8, 14, 25, 42, 48, 53, 58, 59, 61, 77, 79, 84, 95, 96,
 # Total Weight in Dataset: 0.3171 (31.71%)
 
 #1
-vehicles_class = [20, 76, 80, 83, 90, 102, 103, 116, 127]
-'''Car, Boat, Bus, Truck, Airplane, Van, Ship, Minibike, Bicycle '''
-# Total Weight in Dataset: 0.0152 (1.52%)
+flat_surface_class = [3, 5, 6, 11, 52, 54, 91, 94]
+'''Floor, Ceiling, Road, Sidewalk, Path, Runway, Dirt Track, Land, '''
+# Total Weight in Dataset: 0.1683 (16.83%)
 
 #2
-environment_class = [9, 13, 16, 21, 26, 29, 46, 60, 68, 113, 128]
-'''Grass, Earth, Mountain, Water, Sea, Field, Sand, River, Hill, Waterfall, Lake'''
-# Total Weight in Dataset: 0.0669 (6.69%)
-
-#3
 furniture_class = [7, 10, 15, 19, 23, 24, 30, 31, 33, 35, 37, 44, 45, 47, 49, 50, 56, 62, 64, 65, 70, 71, 73, 75, 78, 88, 97, 99, 107, 110, 117, 118, 129, 133, 145, 146]
 '''Bed, Cabinent, Table, Chair, Sofa, Shelf, Armchair, Seat, Desk, Wardrobe, Bathtub, Chest of Drawers, Counter, Sink, Fireplace, Refrigerator,
     Pool Table, Bookcase, Coffee Table, Toilet, Countertop, Stove, Kitchen Island, Swivel Chair, Arcade Machine, Booth, Ottoman, Buffet Counter,
     Wacher, Stool, Cradle, Oven, Dishwasher, Exhaust Hood, Shower, Radiator'''
 # Total Weight in Dataset: 0.1168 (11.8%)
 
+#3
+sky_class = [2]
+'''Sky'''
+# Total Weight in Dataset: 0.0878 (8.78%)
+
 #4
-exterior_object_class = [32, 38, 43, 51, 69, 86, 87, 93, 104, 106, 111, 114, 119, 123, 125, 132, 136, 144, 149]
-'''Fence, Railing, Signboard, Grandstand, Bench, Awning, Streetlight, Pole, Fountain, Canopy, Barrel, Tent, Ball, Brand Name, Pot, Sculpture,
-    Traffic Light, Notice Board, Flag'''
-# Total Weight in Dataset: 0.0173 (1.73%)
+environment_class = [9, 13, 16, 21, 26, 29, 46, 60, 68, 113, 128]
+'''Grass, Earth, Mountain, Water, Sea, Field, Sand, River, Hill, Waterfall, Lake'''
+# Total Weight in Dataset: 0.0669 (6.69%)
 
 #5
 natural_object_class = [4, 17, 34, 66, 72]
@@ -47,16 +46,17 @@ interior_object_class = [18, 22, 27, 28, 36, 39, 40, 41, 55, 57, 63, 67, 74, 81,
 # Total Weight in Dataset: 0.055 (5.5%)
 
 #7
-flat_surface_class = [3, 5, 6, 11, 52, 54, 91, 94]
-'''Floor, Ceiling, Road, Sidewalk, Path, Runway, Dirt Track, Land, '''
-# Total Weight in Dataset: 0.1683 (16.83%)
+exterior_object_class = [32, 38, 43, 51, 69, 86, 87, 93, 104, 106, 111, 114, 119, 123, 125, 132, 136, 144, 149]
+'''Fence, Railing, Signboard, Grandstand, Bench, Awning, Streetlight, Pole, Fountain, Canopy, Barrel, Tent, Ball, Brand Name, Pot, Sculpture,
+    Traffic Light, Notice Board, Flag'''
+# Total Weight in Dataset: 0.0173 (1.73%)
 
 #8
-sky_class = [2]
-'''Sky'''
-# Total Weight in Dataset: 0.0878 (8.78%)
-
-#9
 creatures_class = [12, 126]
 '''Person, Animal'''
 # Total Weight in Dataset: 0.0164 (1.64%)
+
+#9
+vehicles_class = [20, 76, 80, 83, 90, 102, 103, 116, 127]
+'''Car, Boat, Bus, Truck, Airplane, Van, Ship, Minibike, Bicycle '''
+# Total Weight in Dataset: 0.0152 (1.52%)
