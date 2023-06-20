fcdc7c40eadfdef6c525b509c46abe4bdc5b4d47
Sporillium <straussmarch@gmail.com>
2023-06-20 11:01:47 +0200

    Added Display functionality, and ability to read files of parameter pairs

--- parameter_tuning.py
+++ parameter_tuning.py
@@ -6,12 +6,31 @@ import cv2 as cv
 import argparse
 from tqdm import trange, tqdm
 import itertools as iter
+import matplotlib.pyplot as plt
 
 # Custom File Imports:
 import segmentation as seg
 import stereo
 import cloud
 from sem2dp import createSemDescriptorHisto_Tune
+
+# Global Settings
+SMALL_SIZE = 10
+MEDIUM_SIZE = 12
+BIGGER_SIZE = 14
+
+SMALL_WIDTH = 3
+MEDIUM_WIDTH = 4
+BIGGER_WIDTH = 5
+
+plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=BIGGER_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title\
+plt.rc('lines', linewidth=SMALL_WIDTH)  # line width
 
 # Function Definitions
 def generate_decriptor_array(circs, bins, cloud_engine, seq_leng, model):
@@ -41,7 +60,7 @@ def average_precision(prec, rec):
         av_prec += (rec[i+1]-rec[i])*prec[i+1]
     return av_prec
 
-def evaluate_matches(input_descriptors, cloud_ids, distances, des_size, label):
+def evaluate_matches(input_descriptors, cloud_ids, distances, des_size, label, return_data=False):
     matcher = cv.BFMatcher_create(cv.NORM_L2)
     match_boundary = 50 # Number of frames before/after matching can occur
     gt_threshold = 10.0
@@ -97,8 +116,11 @@ def evaluate_matches(input_descriptors, cloud_ids, distances, des_size, label):
             recall.append(0)
     
     mAP = average_precision(precision, recall)
-    
-    return mAP
+
+    if not return_data:
+        return mAP
+    else:
+        return precision, recall, mAP
 
 def eval_Params(params, model):
     (circs, bins) = params
@@ -110,6 +132,16 @@ def eval_Params(params, model):
     print("Descriptor Length: {}".format(des_len))
     print("-------------------------------------")
     return mAP
+
+def define_curves(params, model, axes):
+    (circs, bins) = params
+    label = "@ L="+str(circs)+", T="+str(bins)
+    descriptors, des_len = generate_decriptor_array(circs, bins, cloud_engine, seq_leng, model)
+    cloud_ids = np.arange(seq_leng)
+    precision, recall, mAP = evaluate_matches(descriptors, cloud_ids, distances, des_len, label, return_data=True)
+    plot_key, = axes.plot(recall, precision)
+    plot_key.set_label("mAP="+f'{mAP:.3}'+" "+label)
+    print("-------------------------------------")
 
 def bilinear(circs, bins, vals):
     mid_circ = (circs[1] + circs[0])/2
@@ -147,18 +179,30 @@ def recursive_search(c_min, c_max, b_min, b_max, search_space, model):
     print(search_space)
     return None
     
-
+def read_file(file_name):
+    #print("Reading inputs from "+file_name)
+    pairs = []
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            params = np.fromstring(line, sep=',',dtype=np.int16)
+            param_tuple = (params[0], params[1])
+            pairs.append(param_tuple)
+    return pairs
 
 if __name__ == '__main__':
     # Argument Parser Options:
     parser = argparse.ArgumentParser(description="Perform Parameter Tuning on SeM2DP Models")
     # Required Parameters
-    parser.add_argument('-l', '--circles', required=True, type=int, help="Specify the maximum number of circles to divide Semantic Descriptor into")
-    parser.add_argument('-t', '--bins', required=True, type=int, help="Specify the maximum number of radial bins to divide each circle into")
+    parser.add_argument('-e', '--ex_type', required=True, type=str, help="Specify the execution type, either 'search', 'single', or 'display'")
     parser.add_argument('-n', '--sequence', required=True, type=int, help="Specify the KITTI sequence to use for tuning")
+
     # Other Parameters
     parser.add_argument('-m', '--model', default='normal', type=str, help="Specify the model type, either 'normal', 'super', or 'hamming'")
-    parser.add_argument('-e', '--ex_type', default='search', type=str, help="Specify the execution type, either 'search', or 'single'")
+    parser.add_argument('-f', '--file', default=None, type=str, help="Specify file path to parameter pairs for execution. Not available in 'single' mode")
+    parser.add_argument('-l', '--circles', default=None, type=int, help="Specify the maximum number of circles to divide Semantic Descriptor into")
+    parser.add_argument('-t', '--bins', default=None, type=int, help="Specify the maximum number of radial bins to divide each circle into")
+    
 
     # Parser arguments and check for inconsistencies
     args = parser.parse_args()
@@ -168,21 +212,26 @@ if __name__ == '__main__':
     seq = args.sequence
     mod_type = args.model
     ex_type = args.ex_type
+    file_name = args.file
 
     if mod_type not in ['normal', 'super', 'hamming']:
         print("Undefined Model Type Specified, please choose either: \n\n\tnormal \n\tsuper \n\thamming")
         exit()
-    if ex_type not in ['search', 'single']:
+    if ex_type not in ['search', 'single', 'display']:
         print("Undefined Execution type specified, please choose either 'search' or 'single'")
         exit()
+    
     if seq not in range(11):
         print("Chosen sequence has no ground truth available, please choose from Kitti Sequences 00 thru 10")
         exit()
-    if max_circles < 1:
-        print("Please select a number of circles equal to or greater than 1")
-        exit()
-    if max_bins < 1:
-        print("Please select a number of bins greater than or equal to 1")
+    
+    if max_circles != None and max_bins != None and file_name == None:
+        if max_circles < 1 or max_bins < 1:
+            print("Please select valid parameter ranges if an input file is not specified [L >= 1, T >= 1]")
+            exit()
+    
+    if max_circles == None and max_bins == None and file_name == None:
+        print("Please include either a range of Circles and Bins using the -l and -t switches, or a file of parameters using the -f switch")
         exit()
 
     # Create all of the model required systems:
@@ -192,8 +241,6 @@ if __name__ == '__main__':
     seq_leng = stereo_extractor.seq_len
     seq_name = f'{seq:02}'
 
-   
-
     # Load GT data
     poses = np.loadtxt('/home/march/devel/datasets/Kitti/odometry-2012/poses/'+seq_name+'.txt')
     locations = np.zeros((poses.shape[0], 3))
@@ -201,25 +248,60 @@ if __name__ == '__main__':
         base_pose = np.reshape(poses[0, :], (3,4))
         pose = np.reshape(poses[i, :], (3,4))
         locations[i, :] = pose[:,3]
-    print("\n\nGT POSES LOADED")
+    print("GT poses loaded")
     distances = distance_matrix(locations, locations)
 
     # Check the number of parameter combinations:
     if ex_type == 'search':
-        print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tCIRCLE RANGE:\t1-"+str(max_circles)+"\n\tBIN RANGE:\t1-"+str(max_bins))
-        circles = np.arange(1, max_circles+1)
-        bins = np.arange(1, max_bins+1)
-
-        num_eval = len(circles) * len(bins)
-
-        search_space = np.zeros((len(circles)+1, len(bins)+1))
-
-        # if num_eval > 9:
-        #     print("Using Dynamic Search")
-        #     print(num_eval)
         
-        recursive_search(circles[0], circles[-1], bins[0], bins[-1], search_space, mod_type)
+        if file_name == None:
+            print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"-[range]\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tCIRCLE RANGE:\t1-"+str(max_circles)+"\n\tBIN RANGE:\t1-"+str(max_bins)+"\n")
+            circles = np.arange(1, max_circles+1)
+            bins = np.arange(1, max_bins+1)
+
+            num_eval = len(circles) * len(bins)
+
+            search_space = np.zeros((len(circles)+1, len(bins)+1))
+
+            # if num_eval > 9:
+            #     print("Using Dynamic Search")
+            #     print(num_eval)
+            
+            recursive_search(circles[0], circles[-1], bins[0], bins[-1], search_space, mod_type)
     
+        else:
+            param_list = read_file(file_name)
+            print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"-[file]\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tFILE NAME:\t"+file_name+"\n\tFILE LENGTH:\t"+str(len(param_list))+"\n")
+            for pair in param_list:
+                eval_Params(pair, mod_type)
+
+
     elif ex_type == 'single':
-        print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tCIRCLE VAL:\t"+str(max_circles)+"\n\tBIN VAL:\t"+str(max_bins))
+        print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tCIRCLE VAL:\t"+str(max_circles)+"\n\tBIN VAL:\t"+str(max_bins)+"\n")
         eval_Params((max_circles,max_bins), mod_type)
+    
+    elif ex_type == 'display':
+        # Load Parameters to Evaluate
+        if file_name == None:
+            print("NOT CURRENTLY IMPLEMENTED! PLEASE SPECIFY A FILE WITH DISPLAY MODE")
+        else:
+            param_list = read_file(file_name)
+            print("\n\nPARAMETER SUMMARY:\n\tEVAL TYPE:\t"+ex_type+"-[file]\n\tMODEL TYPE:\t"+mod_type+"\n\tTUNING SEQ:\t"+seq_name+"\n\tFILE NAME:\t"+file_name+"\n\tFILE LENGTH:\t"+str(len(param_list))+"\n")
+            num_plots = len(param_list)
+            plt.gca().set_prop_cycle(plt.cycler('color', plt.cm.jet(np.linspace(0, 1, num_plots))))
+           
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+            ax.set_aspect('equal')
+            ax.set_title("Precision - Recall Curve: Sequence "+seq_name)
+            ax.set_xlabel("Recall")
+            ax.set_ylabel("Precision")
+
+            for pair in param_list:
+                define_curves(pair, mod_type, ax)
+
+            ax.grid()
+            ax.legend()
+            ax.set_xlim([-0.01, 1.01])
+            ax.set_ylim([-0.01, 1.01])
+            plt.show()
